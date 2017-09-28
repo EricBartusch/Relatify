0c7859dc7d8b2e84f40416d2464045daca26991c
Eric Bartusch <ericbartusch8@gmail.com>
2017-09-27 22:56:59 -0500

    Iterative solution

--- getArtists.py
+++ getArtists.py
@@ -10,6 +10,14 @@ driver = GraphDatabase.driver("bolt://localhost:7687", auth=basic_auth("neo4j", 
 session = driver.session()
 session.run("MATCH (n) DETACH DELETE n")
 
+class Artist:
+    def __init__(self, id, name):
+        self.id = id
+        self.name = name
+
+    def __eq__(self, other):
+        return self.id == other.id
+
 def getRelatedArtists(artistId):
     #If we have processed this artist before, skip
     if artistId in artists:
@@ -18,15 +26,37 @@ def getRelatedArtists(artistId):
         artists.append(artistId)
         results = spotify.artist_related_artists(artistId)
         for x in results['artists']:
-            id = x['id']
+            relatedArtistId = x['id']
             name = x['name']
             # if id not in artists:
 
-            print(name + ' - ' + id)
-            session.run("MERGE (a:Artist {name: {name}, id: {id}})", {"name": name, "id": id})
-            session.run("MATCH (a:Artist),(b:Artist) WHERE a.id = '" + artistId + "' AND b.id = '" + id + "' CREATE (a)-[r:RELTYPE { name: a.id + '->' + b.name }]->(b) RETURN r")
-            getRelatedArtists(id)
+            print(name + ' - ' + relatedArtistId)
+            session.run("MERGE (a:Artist {name: {name}, id: {id}})", {"name": name, "id": relatedArtistId})
+            session.run("MATCH (a:Artist),(b:Artist) WHERE a.id = '" + artistId + "' AND b.id = '" + relatedArtistId + "' CREATE (a) -[:relates_to]-> (b)")
+            getRelatedArtists(relatedArtistId)
 
-getRelatedArtists('0OdUWJ0sBjDrqHygGUXeCF')
 
-            
+def getRelatedArtistsIterative(artistId):
+    firstArtistJson = spotify.artist(artistId)
+    firstArtist = Artist(firstArtistJson['id'], firstArtistJson['name'])
+    session.run("MERGE (a:Artist {name: {name}, id: {id}})", {"name": firstArtist.name, "id": firstArtist.id})
+    artists.append(firstArtist)
+    i = 0
+    while 1:
+        if i == len(artists):
+            return
+        results = spotify.artist_related_artists(artists[i].id)
+        for x in results['artists']:
+            relatedArtist = Artist(x['id'], x['name'])
+            if relatedArtist not in artists:
+                artists.append(relatedArtist)
+
+        print(artists[i].name + ' - ' + artists[i].id)
+        for relatedArtist in results['artists']:
+            relatedArtistId = relatedArtist['id']
+            relatedArtistName = relatedArtist['name']
+            session.run("MERGE (a:Artist {name: {name}, id: {id}})", {"name": relatedArtistName, "id": relatedArtistId})
+            session.run("MATCH (a:Artist),(b:Artist) WHERE a.id = '" + artists[i].id + "' AND b.id = '" + relatedArtistId + "' CREATE (a) -[:relates_to]-> (b)")
+        i = i + 1
+
+getRelatedArtistsIterative('0OdUWJ0sBjDrqHygGUXeCF')     
